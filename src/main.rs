319dfd2108410af6c01d253e106ee62385ae559c
Ernesto9139 <ernesto9139@icloud.com>
2025-10-29 16:33:46 -0600

    feat: loop counter

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
 fn main(){
-    let a = 1;
-    let b = 2;
-    let c = 3;
-
-    let res = max_numb(a, b,  c);
-
-    println!("The largerst number is: {}", res);
+    let couter = 5;
+    printer(&couter);
 }
 
-fn max_numb(a: i32, b: i32, c: i32) -> i32 {
-    if a > b {
-        if a > c { a } else { c }
-    } else {
-        if b > c { b } else { c }
+fn printer(a: & i32){ 
+    let mut x = 0;
+    loop{
+        println!("{}", x);
+        x += 1;
+        if x > *a { break; }
     }
 }
-
-
