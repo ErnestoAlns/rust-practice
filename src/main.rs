6b943ba13baedb7e9b44066f2a51eb7a15224546
Ernesto9139 <ernesto9139@icloud.com>
2025-10-30 18:04:23 -0600

    feat: binary search

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
-//    Usa match con rangos para imprimir:
-//    0–12 → "Niño"
-//    13–17 → "Adolescente"
-//    18–64 → "Adulto"
-//    65..=u8::MAX → "Adulto mayor"
-
+use std::cmp::Ordering;
 
 fn main(){
-    let edad = 24;
-    let stat = match &edad {
-        0..=12 => "Niño",
-        13..=17 => "Adolescente",
-        18..=64 => "Adulto",
-        65..=100 => "Adulto mayor",
-        _ => "Error no valido",
-    };
-
-    println!("Tu edad es: {}, entonces eres un {}", edad, stat);
+    let data = vec![1, 2, 5, 7, 8, 9, 10];
+    let target = 8;
+    let index = binary_search(&data, target);
+    match index{
+        Some(index) => {println!("Your target is {} in possition {}", target, index);}
+        None => {println!("Target not found");}
+    }
 }
 
+fn binary_search(data: &[i32], target: i32) -> Option<usize>{
+    let mut down: usize = 0;
+    let mut up: usize = data.len().checked_sub(1)?;
+
+    while down <= up {
+        let mean: usize = down + (up - down) / 2;
+        match data[mean].cmp(&target){
+            Ordering::Less => { down = mean + 1; }
+            Ordering::Greater => { up = mean.checked_sub(1)?; }
+            Ordering::Equal => { return Some(mean); }
+        }
+    }
+    None
+}
